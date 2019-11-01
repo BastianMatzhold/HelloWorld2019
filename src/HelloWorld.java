9ddf19db7abc5921ece6d34d5a4f8896f0c18acc
BastianMatzhold <bastian.matzhold@edu.campus02.at>
2019-11-01 14:48:42 +0100

    Second Commit

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,27 +8,7 @@ public class HelloWorld {
          */
 
         System.out.println("Hello to PR0-World!");
-
-        byte wunschNotePR1 = 1;
-        wunschNotePR1 = 2;
-        System.out.println("Wunschnote:" + wunschNotePR1);
-        wunschNotePR1 = 4;
-        System.out.println("Wunschnote:" + wunschNotePR1);
-
-        boolean istMittwoch = true;
-        System.out.println("Ist heute Mittwoch?" + istMittwoch);
-
-        istMittwoch = false;
-        System.out.println("Ist heute Mittwoch?" + istMittwoch);
-
-        if (istMittwoch == true)
-        {
-            System.out.println("Juhu, es ist PR0");
-        }
-        else
-        {
-            System.out.println("auweh, aber ich übe trotzdem");
-        }
+        System.out.println("Zweite Ausgabezeile");
 
     }
 }
